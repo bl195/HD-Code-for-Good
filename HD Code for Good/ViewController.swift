3ab26707c15dc4cf39710f5a166b8a47a22ffb1c
Brian Li <bl195@duke.edu>
2019-05-29 15:28:37 -0400

    Initial Commit

---
+++ HD Code for Good/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HD Code for Good
+//
+//  Created by Brian Li on 5/29/19.
+//  Copyright © 2019 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
