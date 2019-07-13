c63ab4779eefa30f8238559b1bf6e4900c37fed1
bl195 <bl195@duke.edu>
2019-07-13 14:42:00 -0400

    buttons

--- HD Code for Good/ViewController.swift
+++ HD Code for Good/ViewController.swift
@@ -22,5 +22,98 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    @IBAction func ineqcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func educheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func energycheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func Mobiledevcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func webdevcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func Productpitchcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    
+    @IBAction func frontendcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func Backendcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func Ideabrainstormcheck(_ sender: UIButton){
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func Otherskillcheck(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+        }
+        else{
+            sender.isSelected = true
+        }
+    }
+    
+    
 }
 
