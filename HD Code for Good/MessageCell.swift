5cdff5952d80055bf034223761dd9872d963f6a1
bl195 <bl195@duke.edu>
2019-07-14 15:53:30 -0400

    updates

---
+++ HD Code for Good/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  HD Code for Good
+//
+//  Created by Brian Li on 7/14/19.
+//  Copyright © 2019 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
