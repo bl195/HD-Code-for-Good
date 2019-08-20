e9b3e85214fb7d53a4689440661860d02c212e51
bl195 <bl195@duke.edu>
2019-08-19 20:59:20 -0400

    countdown

--- HD Code for Good/Menu/MenuViewController.swift
+++ HD Code for Good/Menu/MenuViewController.swift
@@ -13,29 +13,41 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var menubut: UIButton!
     
-    let formatter = DateFormatter()
-    let userCalendar = NSCalendar.current
     
-    let requestcomponent: NSCalendar.Unit = [
-        NSCalendar.Unit.month,
-        NSCalendar.Unit.day,
-        NSCalendar.Unit.hour,
-        NSCalendar.Unit.minute,
-        NSCalendar.Unit.second
-    ]
+    let futureDate: Date = {
+        var future = DateComponents(
+            year: 2019,
+            month: 10,
+            day: 13,
+            hour: 0,
+            minute: 0,
+            second: 0
+        )
+       
+        return Calendar.current.date(from: future)!
+    }()
     
+    
+    var countdown: DateComponents {
+        return Calendar.current.dateComponents([.day, .hour, .minute, .second], from: Date(), to: futureDate)
+    }
+    
+    @objc func updateTime() {
+        let countdown = self.countdown //only compute once per call
+        let days = countdown.day!
+        let hours = countdown.hour!
+        let minutes = countdown.minute!
+        let seconds = countdown.second!
+        dateLabel.text = String(format: "%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         menubut.layer.cornerRadius = 20.0
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
-    func printTime(){
-        formatter.dateFormat = "MM/dd/yy hh:mm:ss a"
-        let startTime = NSDate()
-        let endTime = formatter.date(from: "10/13/19 09:00:00 a")
-//        let timedifference = userCalendar.dateComponents(requestcomponent, from: startTime, to: endTime)
-    }
-   
-
+    
 }
