8fd1192ce575c064c0c51704773ca0cc131801a6
bl195 <bl195@duke.edu>
2019-08-25 23:42:31 -0400

    map

--- HD Code for Good/Map/MapScreen.swift
+++ HD Code for Good/Map/MapScreen.swift
@@ -1,85 +1,115 @@
-//
-//  MapScreen.swift
-//  HD Code for Good
-//
-//  Created by Brian Li on 7/9/19.
-//  Copyright © 2019 Brian Li. All rights reserved.
-//
+
+
 import UIKit
 import MapKit
 import CoreLocation
+
+final class Annotation: NSObject, MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?){
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        
+        super.init()
+    }
+    
+    var region: MKCoordinateRegion{
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        return MKCoordinateRegion(center: coordinate, span: span)
+    }
+    
+}
 
 class MapScreen: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
-    let locationManager = CLLocationManager()
-    let regionInMeters: Double = 10000
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkLocationServices()
-      
-    }
-
-func setupLocationManager() {
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-}
-
-
-func centerViewOnUserLocation() {
-    if let location = locationManager.location?.coordinate {
-        let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        let chapelCoordinate = CLLocationCoordinate2D(latitude: 36.000481, longitude: -78.937332)
+        let chapelAnnotation = Annotation(coordinate: chapelCoordinate, title: "Chapel", subtitle: "Most Iconic Duke Structure")
+        mapView.addAnnotation(chapelAnnotation)
+        mapView.setRegion(chapelAnnotation.region, animated: true)
+//        checkLocationServices()
     }
 }
 
-
-func checkLocationServices() {
-    if CLLocationManager.locationServicesEnabled() {
-        setupLocationManager()
-        checkLocationAuthorization()
-    } else {
-        // Show alert letting the user know they have to turn this on.
+    extension ViewController: MKMapViewDelegate{
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+            if let chapelAnnotateView = mapView.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier) as? MKMarkerAnnotationView {
+                chapelAnnotateView.animatesWhenAdded = true
+                chapelAnnotateView.titleVisibility = .adaptive
+                chapelAnnotateView.titleVisibility = .adaptive
+                
+                return chapelAnnotateView
+        }
+        return nil
     }
+
+//func setupLocationManager() {
+//    locationManager.delegate = self
+//    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//}
+////
+////
+//func centerViewOnUserLocation() {
+//    if let location = locationManager.location?.coordinate {
+//        let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
+//}
+////
+////
+//func checkLocationServices() {
+//    if CLLocationManager.locationServicesEnabled() {
+//        setupLocationManager()
+//        checkLocationAuthorization()
+//    } else {
+//        // Show alert letting the user know they have to turn this on.
+//    }
+//}
+////
+////
+//func checkLocationAuthorization() {
+//    switch CLLocationManager.authorizationStatus() {
+//    case .authorizedWhenInUse:
+//        mapView.showsUserLocation = true
+//        centerViewOnUserLocation()
+//        locationManager.startUpdatingLocation()
+//        break
+//    case .denied:
+//        // Show alert instructing them how to turn on permissions
+//        break
+//    case .notDetermined:
+//        locationManager.requestWhenInUseAuthorization()
+//    case .restricted:
+//        // Show an alert letting them know what's up
+//        break
+//    case .authorizedAlways:
+//        break
+//    }
+//}
+////
+////}
+////
+//extension MapScreen: CLLocationManagerDelegate {
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        checkLocationAuthorization()
+//    }
 }
 
 
-func checkLocationAuthorization() {
-    switch CLLocationManager.authorizationStatus() {
-    case .authorizedWhenInUse:
-        mapView.showsUserLocation = true
-        centerViewOnUserLocation()
-        locationManager.startUpdatingLocation()
-        break
-    case .denied:
-        // Show alert instructing them how to turn on permissions
-        break
-    case .notDetermined:
-        locationManager.requestWhenInUseAuthorization()
-    case .restricted:
-        // Show an alert letting them know what's up
-        break
-    case .authorizedAlways:
-        break
-    }
-}
-
-}
-
-extension MapScreen: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
-    }
-
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        checkLocationAuthorization()
-    }
-}
-    
 
 
