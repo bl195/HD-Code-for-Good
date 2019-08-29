cc69fe9db427c3c891f4a4dd88eb38929f37d3c5
bl195 <bl195@duke.edu>
2019-08-29 12:13:56 -0400

    stuf

---
+++ HD Code for Good/Check-In/CheckedInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CheckedInViewController.swift
+//  HD Code for Good
+//
+//  Created by Brian Li on 8/29/19.
+//  Copyright © 2019 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class CheckedInViewController: UIViewController {
+
+    @IBOutlet weak var backbut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backbut.layer.cornerRadius = 20.0
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
