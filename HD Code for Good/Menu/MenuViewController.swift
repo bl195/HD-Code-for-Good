e435e83612a01b72541d98ddfee07466bb7511a3
bl195 <bl195@duke.edu>
2019-10-27 13:42:55 -0400

    colors

--- HD Code for Good/Menu/MenuViewController.swift
+++ HD Code for Good/Menu/MenuViewController.swift
@@ -34,7 +34,32 @@ class MenuViewController: UIViewController {
 
     @IBOutlet weak var checkedinlabel: UILabel!
     
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    
     override func viewDidLoad() {
+        var bgcolor = hexStringToUIColor(hex: "#FFD38D")
+        self.view.backgroundColor = bgcolor
         print(checkedin)
         if checkedin == true{
             menubut.isHidden = true
